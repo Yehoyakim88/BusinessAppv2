2f7076e16d63faa2a845429888d61e0e8ed6cbe1
Yehoyakim88 <yehoyakim@mail.de>
2023-03-08 15:56:12 +0100

    new toolbar setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TooltipPosition } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Business App';
   showFiller = false;
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[0]);
+  below = new FormControl(this.positionOptions[1]);
+  public isVisited = false; 
+
+  initiateSearch() {
+    let searchBar : HTMLElement = document.getElementById('searchbar-components') as HTMLElement;
+    let toolBar : HTMLElement = document.getElementById('toolbar-components') as HTMLElement;
+    searchBar.classList.remove('d-none');
+    toolBar.classList.add('d-none');
+  }
+
+  closeSearch() {
+    let searchBar : HTMLElement = document.getElementById('searchbar-components') as HTMLElement;
+    let toolBar : HTMLElement = document.getElementById('toolbar-components') as HTMLElement;
+    searchBar.classList.add('d-none');
+    toolBar.classList.remove('d-none');
+  }
+
+  checkVisited() {
+    console.log('Calling function checkVisited()');
+    this.isVisited = !this.isVisited;
+    console.log('isVisited: ', this.isVisited);
+  }
 }
